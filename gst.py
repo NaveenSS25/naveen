9a37143cb5d80333c724ed890fbed8bc1df1f0ef
NaveenSS25 <61132652+NaveenSS25@users.noreply.github.com>
2020-03-16 11:32:01 +0530

    Update gst.py

--- gst.py
+++ gst.py
@@ -10,3 +10,24 @@ TWGST=total+sgst+cgst
 print("Total With GST: ",TWGST)
 from num2words import num2words
 print(num2words(TWGST to ='currency'))
+
+
+
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    int sum,a,b,c,d,e,f;
+	    Scanner s=new Scanner(System.in);
+	    a=s.nextInt();
+	    sum=a%10;
+	    b=a/10;
+	    c=b+sum;
+	    d=c%10;
+	    e=c/10;
+	    f=d+e;
+		System.out.println("" + f);
+	}
+}
